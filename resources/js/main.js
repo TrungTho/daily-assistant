8584a4506fa1882b8a62a8c43fce67e85ddd0588
Tho Tran <Tho_Tran@mckinsey.com>
2023-09-24 18:11:10 +0700

    feature: simple water drinking countdown

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,38 @@
-let getUsername = async () => {
-    const key = NL_OS == 'Windows' ? 'USERNAME' : 'USER';
-    let value = '';
-    try {
-        value = await Neutralino.os.getEnv(key);
+const DEFAULT_COUNTDOWN_DURATION = 900; //15 minutes
+let timer = DEFAULT_COUNTDOWN_DURATION;
+const notificationSound = new Audio("./sounds/notification-sound.wav")
+
+const startCountdown = async () => {
+
+    setInterval(async () => {
+        await displayCountdownTimer();
+    }, 1000);
+}
+
+const timeFormatter = async (timeInSeconds) => {
+    const date = new Date(timeInSeconds * 1000);
+    if (timeInSeconds < 3600) {
+        return date.toISOString().substring(14, 19);
     }
-    catch (err) {
-        console.error(err);
+
+    return date.toISOString().substring(11, 19);
+}
+
+const displayCountdownTimer = async () => {
+    timer -= 1;
+    const formattedTime = await timeFormatter(timer);
+    document.getElementById('timer-display').innerText = `${formattedTime}`;
+
+    if (timer < 1) {
+        await showWaterNotification()
+        timer = DEFAULT_COUNTDOWN_DURATION + 1;
     }
-    document.getElementById('name').innerText = `Hello ${value}`;
+}
+
+const showWaterNotification = async () => {
+    await notificationSound.play();
+    await Neutralino.os.showNotification('Water', 'Drink some water please!!!');
 }
 
 Neutralino.init();
-getUsername();
+startCountdown();
