9a7447f73c3575537470bc4f73895d12a5f93f95
TrungTho <41388666+TrungTho@users.noreply.github.com>
2023-10-01 16:16:59 +0700

    feature: basic countdown timer for water drinking (#2)
    * feature: simple water drinking countdown
    
    * chore: update app name to display
    
    * feature: add voice for water notification
    
    * fix: change logic to prevent incorrect interval count
    
    * Test refactor
    
    ---------
    
    Co-authored-by: Tho Tran <Tho_Tran@mckinsey.com>
    Co-authored-by: Chi Ma <chigia001@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# daily-assistant
-Mini app with NeutralinoJS to support with daily needs
+# Daily Assistant
+
+Lightweight desktop mini app powered by NeutralinoJS to help you with your daily needs.
+
+# Sounds
+
+Notification sounds: https://mixkit.co/free-sound-effects/
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,14 +1,72 @@
-let getUsername = async () => {
-    const key = NL_OS == 'Windows' ? 'USERNAME' : 'USER';
-    let value = '';
-    try {
-        value = await Neutralino.os.getEnv(key);
-    }
-    catch (err) {
-        console.error(err);
-    }
-    document.getElementById('name').innerText = `Hello ${value}`;
+const DEFAULT_COUNTDOWN_DURATION = 30; //30 seconds
+const getFinishedTime = (preTime = new Date())  => new Date(preTime.getTime() + 1000 * DEFAULT_COUNTDOWN_DURATION)
+let finishTime = getFinishedTime();
+
+const initAudio = src => {
+    const audio = new Audio(src)
+    audio.muted = true;
+    audio.autoplay = false;
+    return audio;
+}
+const notificationSound = initAudio('./sounds/notification-sound.wav');
+const waterVoice = initAudio('sounds/water-voice.mp3')
+const timerDisplayElm = document.getElementById('timer-display')
+
+const startCountdown = () => {
+    setInterval(() => {
+        displayCountdownTimer();
+    }, 500); // 0.5 second interval
 }
 
-Neutralino.init();
-getUsername();
+const timeFormatter = (timeInSeconds) => {
+    const date = new Date(timeInSeconds * 1000);
+    if (timeInSeconds < 3600) {
+        return date.toISOString().substring(14, 19);
+    }
+
+    return date.toISOString().substring(11, 19);
+}
+
+const displayCountdownTimer = () => {
+    const currTime = new Date();
+    if (currTime >= finishTime) {
+        showWaterNotification(currTime)
+        finishTime = getFinishedTime(finishTime);
+    }
+
+    const duration = (finishTime - currTime) / 1000;
+    const formattedTime = timeFormatter(duration);
+    timerDisplayElm.innerText = `${formattedTime}`;
+}
+
+const showWaterNotification = (currTime) => {
+    Neutralino.debug.log(`${currTime.toISOString()}: Notification shown`);        
+    playAudio(notificationSound).then(() => playAudio(waterVoice)).then(() => Neutralino.debug.log(`${currTime.toISOString()}: Audio Finished`))
+    Neutralino.os.showNotification('Water', 'Drink some water please!!!');
+}
+
+const waitAudioToLoad = (audio) => new Promise((resolve) => {
+    audio.addEventListener("canplaythrough", resolve, {
+        once: true
+    })
+})
+
+const playAudio = async (audio) => {
+    audio.muted = false;
+    await new Promise((resolve) => {
+        audio.addEventListener("ended", resolve, {
+            once: true
+        })
+        audio.play()
+    })
+    audio.pause()
+    audio.muted = true;
+};
+
+Promise.all([
+    waitAudioToLoad(notificationSound),
+    waitAudioToLoad(waterVoice)
+]).then(() => {
+    Neutralino.init();
+    startCountdown();
+})
